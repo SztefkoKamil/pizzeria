a1aa7ace1f3fc36f2c9f773730668bf304c7e791
Kamil Sztefko <sztefkokamil@gmail.com>
2018-11-29 07:50:44 +0100

    Google Maps Location ADD

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,13 +4,13 @@
       <div>
         <h2>Zapraszamy do odwiedzin lub kontaktu telefonicznego.</h2>
         <p>Lokal czynny przez cały tydzień od 10 do 22.</p>
+        <p>Dowóz w promieniu 20km gratis!</p>
       </div>
       <div>
         <p>Telefon: 11 222 33 44</p>
         <p>Adres: Krakowska 40, Kozy 43-340</p>
       </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d643.1792527455344!2d19.148930929271692!3d49.8477745987123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716993b81586a4d%3A0x5bbccf8169b5cd6d!2sKrakowska+40%2C+43-340+Kozy!5e0!3m2!1spl!2spl!4v1543467049360" width="700" height="400" frameborder="0" style="border:0"></iframe>
-      <!-- <div id="map"></div> -->
+      <div id="map"></div>
       <div>
         <h3>Masz pytania? Napisz do nas!</h3>
         <form>
