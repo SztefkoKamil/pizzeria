d1487787cf0a0625657c860871a4a86334cad5b6
Kamil Sztefko <sztefkokamil@gmail.com>
2018-12-09 09:05:50 +0100

    slider images output path CHANGE

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -4,31 +4,31 @@
     <div class="swiper-wrapper">
       <swiper :options="swiperOption" class="swiper-box">
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-1.jpg" alt="slider pizza image 1">
+          <img src="../assets/img/slide/slide-pizza-1.jpg" alt="slider pizza image 1">
         </swiper-slide>
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-2.jpg" alt="slider pizza image 2">
+          <img src="../assets/img/slide/slide-pizza-2.jpg" alt="slider pizza image 2">
         </swiper-slide>
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-3.jpg" alt="slider pizza image 3">
+          <img src="../assets/img/slide/slide-pizza-3.jpg" alt="slider pizza image 3">
         </swiper-slide>
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-4.jpg" alt="slider pizza image 4">
+          <img src="../assets/img/slide/slide-pizza-4.jpg" alt="slider pizza image 4">
         </swiper-slide>
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-5.jpg" alt="slider pizza image 5">
+          <img src="../assets/img/slide/slide-pizza-5.jpg" alt="slider pizza image 5">
         </swiper-slide>
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-6.jpg" alt="slider pizza image 6">
+          <img src="../assets/img/slide/slide-pizza-6.jpg" alt="slider pizza image 6">
         </swiper-slide>
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-7.jpg" alt="slider pizza image 7">
+          <img src="../assets/img/slide/slide-pizza-7.jpg" alt="slider pizza image 7">
         </swiper-slide>
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-8.jpg" alt="slider pizza image 8">
+          <img src="../assets/img/slide/slide-pizza-8.jpg" alt="slider pizza image 8">
         </swiper-slide>
         <swiper-slide class="swiper-item">
-          <img src="src/assets/img/slide/slide-pizza-9.jpg" alt="slider pizza image 9">
+          <img src="../assets/img/slide/slide-pizza-9.jpg" alt="slider pizza image 9">
         </swiper-slide>
       </swiper>
     </div>
